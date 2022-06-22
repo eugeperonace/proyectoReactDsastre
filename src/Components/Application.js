356b1269847de9e64143f59a7ee5283c9fd9afda
Eugenia Peronace <eugeperonace@gmail.com>
2022-06-22 19:48:26 -0300

    Desafío 2

---
+++ src/Components/Application.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logoGrande from '.././assets/LogoGrande.jpg'
+
+const Application = () =>{
+    return(
+        <>
+            <img style={styles.image} src={logoGrande} alt="DSASTRE" />
+            
+        </>
+    );
+}
+
+const styles = {
+    image: {
+        //alignSelf: 'center',
+        paddingLeft: '30%',
+        paddingRight: '30%',
+    },
+}
+
+export default Application
